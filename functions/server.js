1d91e9b80a86814d194768c5322b9bda6397cd3b
Johan <kwcjohan@outlook.com>
2025-08-17 22:08:41 +0700

    fix servless server.js

---
+++ functions/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => res.json({ message: 'Hello from Netlify!' }));
+app.use('/.netlify/functions/server', router);
+
+module.exports.handler = serverless(app);
